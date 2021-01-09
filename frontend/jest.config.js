f004f532d6219bbc6b9fcf8a0e6f6130e49925a3
Pavel Mineev <pavel@mineev.me>
2021-01-10 00:03:25 +0300

    reorganize auth

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -13,15 +13,18 @@ module.exports = {
      * "transformIgnorePatterns" just don't work for modules down below
      * If you know how to handle it better PR welcome
      */
-    '^@github/markdown-toolbar-element$': 'identity-obj-proxy',
-    '^@github/text-expander-element$': 'identity-obj-proxy',
+    // '^@github/markdown-toolbar-element$': 'identity-obj-proxy',
+    // '^@github/text-expander-element$': 'identity-obj-proxy',
   },
   setupFiles: ['<rootDir>/jest.setup.ts'],
   setupFilesAfterEnv: [
-    'jest-localstorage-mock',
-    '<rootDir>/app/__mocks__/headers.ts',
+    '<rootDir>/app/__mocks__/localstorage.ts',
+    '<rootDir>/app/__mocks__/fetch.ts',
     '<rootDir>/app/__stubs__/remark-config.ts',
     '<rootDir>/app/__stubs__/static-config.ts',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(lodash-es|@github/markdown-toolbar-element|@github/text-expander-element)/)',
   ],
   collectCoverageFrom: ['!**/__mocks__/**', '!**/__stubs__/**', '!app/locales/**'],
   globals: {
